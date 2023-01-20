6ec4cd52841aad3b90d342cb85cd2093d1fc6f75
Jeewantha <chathurajmhb7@gmail.com>
2023-01-20 12:44:06 +0530

    Add Project Files

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SquareCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            int lowerLimit = 0;
+            int upperLimit = 0;
+            int countBy = 1;
+
+            listBox1.Items.Clear();
+
+            try
+            {
+                lowerLimit = int.Parse(txt_lowerLimit.Text);
+                upperLimit = int.Parse(txt_upperLimit.Text);
+                countBy = int.Parse(txt_countBy.Text);
+
+                if(lowerLimit > upperLimit)
+                {
+                    // swap
+                    int temp = lowerLimit;
+                    lowerLimit = upperLimit;
+                    upperLimit = temp;
+                }
+
+                for(int x = lowerLimit; x <= upperLimit; x = x + countBy)
+                {
+                    listBox1.Items.Add("x = " + x + " Square = " + x*x);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Try only numbers please!");
+            }
+
+            txt_lowerLimit.Clear();
+            txt_upperLimit.Clear();
+            txt_countBy.Clear();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+## Square Calculator (Mini Calculator)
+
+Square Calculator is a mini calculator using C#. Need to enter the lower limit, upper limit and count by values. After enter the inputs client able to get square of the numbers in between lower and upper limit.
+
+## Screen Shots
+<img src="ReadMe/img1.png" width="600"><br>
+<img src="ReadMe/img2.png" width="600"><br>
+<img src="ReadMe/img3.png" width="600"><br>
